6d31572ed99a5054664de829551a765170281e31
Yashraj <info.aopstsma@gmail.com>
2026-09-10 22:53:12 +0530

    Fix school directory text contrast: make school names crisp white by default and warm gold on hover

--- app/schools/SchoolsClient.tsx
+++ app/schools/SchoolsClient.tsx
@@ -102,8 +102,8 @@ export default function SchoolsClient() {
           {filteredSchools.length > 0 ? (
             filteredSchools.map((s, idx) => (
               <tr key={idx}>
-                <td style={{ color: '#64748B', fontWeight: 600 }}>{s.slNo || idx + 1}</td>
-                <td data-l="School" style={{ fontWeight: 600, color: '#0B2545' }}>{s.name}</td>
+                <td style={{ color: '#94A3B8', fontWeight: 600 }}>{s.slNo || idx + 1}</td>
+                <td data-l="School" style={{ fontWeight: 600 }}>{s.name}</td>
                 <td data-l="Zone">
                   <Link
                     href={`/schools?zone=${s.zone}`}
